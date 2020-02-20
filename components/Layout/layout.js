61886a7ffe7267d992f4d07616bbbaaef3241f4c
MOREAU Eliott <moreaueliott49@gmail.com>
2020-02-20 17:00:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './layout.scss';
 import Header from '../Header/header';
-import Footer from '../Footer/Footer';
+import Footer from '../Footer/footer';
 
 const Layout = (props) => {
     return(
