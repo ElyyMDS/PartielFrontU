c562bc0738a96012ae83b431352fef27ec063f50
Alexandre <alex_didi49@hotmail.com>
2020-02-20 16:53:22 +0100

    :construction: adding ( footer )

---
+++ components/Footer/footer.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './footer.scss';
+import Link from 'next/link';
+
+const Footer = () => {
+    return(
+        <footer className="block_footer">
+            <div className="footer_wrapper">
+                <ul className="footer_menu">
+                    <li className="footer_item">
+                        <Link href="/conditions">
+                            <a className="footer_link">
+                                <p className="fotoer_texte">Plan du site</p>
+                            </a>
+                        </Link>
+                    </li>
+                    <li className="footer_item">
+                        <Link href="/conditions">
+                            <a className="footer_link">
+                                <p className="fotoer_texte">Conditions d'utilisation</p>
+                            </a>
+                        </Link>
+                    </li>
+                    <li className="footer_item">
+                        <Link href="/conditions">
+                            <a className="footer_link">
+                                <p className="fotoer_texte">Confidentialité</p>
+                            </a>
+                        </Link>
+                    </li>
+                    <li className="footer_item">
+                        <Link href="/conditions">
+                            <a className="footer_link">
+                                <p className="fotoer_texte">FAQ</p>
+                            </a>
+                        </Link>
+                    </li>
+                    <li className="footer_item">
+                        <Link href="/conditions">
+                            <a className="footer_link">
+                                <p className="fotoer_texte">Aide</p>
+                            </a>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="footer_social">
+                    <ul className="footer_social_menu">
+                        <li className="footer_social_item"><Link href="#" ><a><img src="assets/twitter.svg" /></a></Link></li>
+                        <li className="footer_social_item"><Link href="#" ><a><img src="assets/instagram.svg" /></a></Link></li>
+                        <li className="footer_social_item"><Link href="#" ><a><img src="assets/facebook.svg" /></a></Link></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- pages/Index/index.js
+++ pages/Index/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './index.scss';
+import Footer from '../../components/Footer/Footer';
 
 const Index = () => {
     return(
         <div>
             <h1 className="title">Index</h1>
+            <Footer />
         </div>
     );
 };
