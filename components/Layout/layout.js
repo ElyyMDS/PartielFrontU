2a7031e2e48014edda654b38c98c8e1464ff43f2
Alexandre <alex_didi49@hotmail.com>
2020-02-20 16:59:25 +0100

    :construction: update ( Layout ) adding Footer

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './layout.scss';
 import Header from '../Header/header';
+import Footer from '../Footer/Footer';
 
 const Layout = (props) => {
     return(
         <div>
             <Header />
             {props.children}
+            <Footer />
         </div>
     ); 
 }
