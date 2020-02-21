7604df8cc99ca72b64f75e099d09c13df1005643
Alexandre <alex_didi49@hotmail.com>
2020-02-21 09:47:42 +0100

    :construction: update ( user , bash ) adding generate file

--- pages/Index/index.js
+++ pages/Index/index.js
@@ -3,18 +3,18 @@ import {connect} from "react-redux";
 import Layout from '../../components/Layout/layout';
 import * as action from '../../redux/actions/user_actions';
 
-const Page = props => {
+const Index = props => {
     return(
-        <Layout>
+        <Layout >
             <div>Prop from getInitialProps {props.custom}</div>
-            <p onClick={() => props.dispatch(action.register('jao_izumu','123','jao@gmail.com'))}>essaye moi</p>
+            <p onClick={() => props.dispatch({type: 'INCREMENT'})}>essaye moi</p>
         </Layout>
     );
 }
 
-Page.getInitialProps = ({store, isServer, pathname, query}) => {
+Index.getInitialProps = ({store, isServer, pathname, query}) => {
 
     return {custom: 'custom'}; // You can pass some custom props to the component from here
 }
 
-export default connect(state => state)(Page);
+export default connect(state => state)(Index);
--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,9 +1,9 @@
-export default function todos(state = {}, action) {
+export default function reducer(state = {}, action) {
     switch (action.type) {
       case 'USER_LOGIN_SUCCESS':
         console.log(action.payload);
         return state
-    case 'USER_REGISTER_SUCCESS':
+      case 'USER_REGISTER_SUCCESS':
         // user exist
         if(action.payload.status === 409){
             console.log(action.payload)
