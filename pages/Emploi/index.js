60e6cdd22a6bdfd479e82a5309ace5ebae9b5c5a
MOREAU Eliott <moreaueliott49@gmail.com>
2020-02-25 13:53:59 +0100

    :construction: updating (emploi) adding search bar

--- pages/Emploi/index.js
+++ pages/Emploi/index.js
@@ -4,6 +4,7 @@ import Layout from '../../components/Layout/layout';
 import * as action from '../../redux/actions/user_actions';
 import "./emploi.scss";
 import CardEmploi from "../../components/CardEmploi/cardEmploi";
+import Button from "../../components/Button/button";
 
 const Emploi = props => {
     const cardEmploi = [
@@ -18,6 +19,19 @@ const Emploi = props => {
         <Layout>
             <main className="block_emploi">
                 <h2>Offres d'emplois</h2>
+                <form className="form_search">
+                    <div class="input-icons"> 
+                        <i class="fa fa-search icon"></i> 
+                        <input class="input-field" type="text" placeholder="Poste" />
+                    </div> 
+                    <div class="input-icons"> 
+                        <i class="fa fa-map-marker icon"></i> 
+                        <input class="input-field" type="text" placeholder="Localisation" />
+                    </div> 
+                    <div className="button">
+                        <Button href="#" text="Rechercher" textAlign="center" color="#FFC30B" border="2px solid #FFC30B"/>
+                    </div>
+                </form>
                 <div className="wrapper">
                 {cardEmploi.map((card, index) => {
                             return <CardEmploi key={index} img={card.img} title={card.title} type={card.type} content={card.content} salaire={card.salaire} localisation={card.localisation}/>
